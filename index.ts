3918fabaf97ed46805143834e48fbafeb465703a
Nicholas Heger <nicholas.heger@zirous.com>
2021-12-14 14:18:35 -0600

    strongly typing the ModuleWithProviders forRoot()

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ export * from './src/slim-loading-bar.service';
     providers: [SlimLoadingBarService]
 })
 export class SlimLoadingBarModule {
-    static forRoot(): ModuleWithProviders<any> {
+    static forRoot(): ModuleWithProviders<SlimLoadingBarModule> {
         return {
             ngModule: SlimLoadingBarModule,
             providers: [SlimLoadingBarService]
